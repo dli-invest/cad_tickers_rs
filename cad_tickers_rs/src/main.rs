bda2f06a72077e39806095508188c31390885f42
David Li <davidli012345@gmail.com>
2023-09-06 12:14:40 -0700

    feat: try again

--- cad_tickers_rs/src/main.rs
+++ cad_tickers_rs/src/main.rs
@@ -32,3 +32,32 @@ async fn send_request(payload: serde_json::Value) -> Result<String, Box<dyn std:
     let body = res.text().await?;
     Ok(body)
 }
+
+fn main() {
+
+    let symbol = "AAPL";
+    
+    // Get ticker filings
+    let gql = GQL::new();
+    let mut payload = gql.get_company_filings_payload();
+    payload["variables"]["symbol"] = json!(symbol);
+    
+    let filings_result = get_ticker_filings(&payload).await;
+    
+    println!("Ticker filings response: {}", filings_result.unwrap());
+  
+    
+    // Get news and events
+    
+    let mut news_payload = gql.get_company_news_events_payload();
+    news_payload["variables"]["symbol"] = json!(symbol);
+    news_payload["variables"]["page"] = json!(1);
+    news_payload["variables"]["limit"] = json!(10);
+    news_payload["variables"]["locale"] = json!("en-CA");
+    
+    let news_result = get_news_and_events(&news_payload).await;
+  
+    println!("News and events response: {}", news_result.unwrap());
+  
+}
+  
